a90bcf7162dfa75e3ec564ca1fbd7339160fb5c6
Trey Fletcher <trey.fletcher@linuxacademy.com>
2020-05-28 14:09:36 -0400

    passing tests

--- server.js
+++ server.js
@@ -2,7 +2,19 @@ let express = require('express');
 let app = express();
 let port = 8080
 
-app.use('/', express.static('public'))
+app.get('/healthcheck', (req, res) => {
+    res.json('App is alive')
+})
+app.use(express.static('public'))
+app.use('/', (req, res) => {
+    res.sendFile("index.html")
+})
+
+app.use('/hello', (req, res) => {
+    res.sendFile("hello.html")
+})
 
 let server = app.listen(port);
 console.log(`Server listening on port ${port}`)
+
+module.exports = app;
--- test/test-pages.js
+++ test/test-pages.js
@@ -1,9 +1,27 @@
-var expect  = require('chai').expect;
-var request = require('request');
+const expect = require('chai').expect
+const request = require('supertest')
+const app = require('../server.js')
 
-it('Tests for Hello World', function(done) {
-    request('http://localhost:8080' , function(error, response, body) {
-        expect(body).to.equal('Hello World!');
-        done();
-    });
-});
+describe('GET /healthcheck',() => {
+    it('err should be null', (done) => {
+        request(app).get('/healthcheck')
+        .end((err,res) => {
+            expect(err).to.be.null
+            done()
+        })
+    })
+    it('returns 200 status', (done) => {
+        request(app).get('/healthcheck')
+        .end((err,res) => {
+            expect(res.statusCode).to.equal(200)
+            done()
+        })
+    })
+    it('body includes App is alive', (done) => {
+        request(app).get('/healthcheck')
+        .end((err,res) => {
+            expect(res.body).to.equal('App is alive')
+            done()
+        })
+    })
+})
